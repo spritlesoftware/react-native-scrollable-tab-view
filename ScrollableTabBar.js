5e07521895e3501de13811af45b8b42e6803bdcb
Hariraj <raja70a@gmail.com>
2018-04-17 19:34:02 +0530

    Fontweight removed in ScrollableTabbar

--- ScrollableTabBar.js
+++ ScrollableTabBar.js
@@ -127,7 +127,6 @@ const ScrollableTabBar = createReactClass({
   renderTab(name, page, isTabActive, onPressHandler, onLayoutHandler) {
     const { activeTextColor, inactiveTextColor, textStyle, } = this.props;
     const textColor = isTabActive ? activeTextColor : inactiveTextColor;
-    const fontWeight = isTabActive ? 'bold' : 'normal';
 
     return <Button
       key={`${name}_${page}`}
@@ -138,7 +137,7 @@ const ScrollableTabBar = createReactClass({
       onLayout={onLayoutHandler}
     >
       <View style={[styles.tab, this.props.tabStyle, ]}>
-        <Text style={[{color: textColor, fontWeight, }, textStyle, ]}>
+        <Text style={[{color: textColor }, textStyle, ]}>
           {name}
         </Text>
       </View>
